8ada6f50dacac238dd2136e00483dd96209f6f1d
Gisel Paye Luna <cecipaye@gmail.com>
2022-09-08 15:25:04 -0400

    mongoose server

--- npmtest/server.js
+++ npmtest/server.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
+const Product = require("./models/Product");
 const app = require("./app");
 const port = process.env.PORT || 3032;
-mongoose.connect(process.env.DATABASE, {}).then((con)=> {
-    console.log("Connected--------", con.connections);
+const url= 'mongodb://localhost:27017/fullstack';
+mongoose.connect(url, {}).then((con) => {
+    console.log("Connected to mongo");
+    const p = new Product({ productName:"producto 2", price: 10});
+    p.save().then(() => {
+        console.log("saved");
+    });
 });
 app.listen(port, () =>{
     console.log(`App running on port ${port}`);
