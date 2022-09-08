a73e3c284b6e1f5560262555c4f110a894ce59ea
Gisel Paye Luna <cecipaye@gmail.com>
2022-09-08 19:35:33 -0400

    terminado branch mongo

--- npmtest/controllers/productController.js
+++ npmtest/controllers/productController.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
+const Product = require("../models/Product");
 //const path = require('path');
 
-exports.getAllProducts = (req, res) => {
-   // console.log(req.params);
-    const products = JSON.parse(
-      fs.readFileSync(`${__dirname}/../data/products.json`)
-      );
-    //console.log(req.requestTime);
+exports.getAllProducts = async (req, res) => {
+
+    const products = await Product.find();
+    console.log(products);
     res.status(200).json({
       status: "success",
       timeOfRequest: req.requestTime,
@@ -14,36 +13,30 @@ exports.getAllProducts = (req, res) => {
       data: {
         products,
       },
+      
     });
   }
   
-  exports.addproduct = (req, res) => {
-    const products = JSON.parse(
-      fs.readFileSync(`${__dirname}/../data/products.json`)
-      );
-    products.push(req.body);
-    fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+  exports.addproduct = async(req, res) => {
+    //const product = new Product({});
+    const newProduct = await Product.create(req.body);
     res.status(200).json({
       status: "success",
       data: {
-        products,
+        product: newProduct,
       },
     });
   }
   
-  exports.getProductById = (req, res) => {
-    const products = JSON.parse(
-      fs.readFileSync(`${__dirname}/../data/products.json`)
-      );
-  
-    const foundProduct = products.find(p => p.id == req.params.id);
+  exports.getProductById = async(req, res) => {
+    const foundProduct = await Product.findById(req.params.id);
     //console.log(req.params);
     if(foundProduct){
 
       res.status(200).json({
         status: "success",
         data: {
-          products: foundProduct,
+          product: foundProduct,
         },
       });
     } else{
@@ -51,41 +44,34 @@ exports.getAllProducts = (req, res) => {
           status: "not found",
         });
     }
-  }
+  };
   
-  exports.updateProductById = (req, res) => {
-    const products = JSON.parse(fs.readFileSync(`${__dirname}/../data/products.json`));
+  exports.updateProductById = async(req, res) => {
+    const product = await Product.findById(req.params.id);
     
-    let edito = false;
-    for (const producto of products) {
-      console.log(producto.id, req.params.id);
-    
-      if (producto.id == req.params.id) {
-        edito = true;
-        producto.name = req.body.name;
-        producto.price = req.body.price;
-        producto.category = req.body.category;
+      if (product) {
+          product.productName = req.body.productName;
+          product.price = req.body.price;
+          res.status(200).json({
+            status: "success",
+            data: {
+              product: product,
+            }
+          });
+      }else{
+        res.status(404).json({
+          status: "not found",
+        });
       }
-    }
-
-    if (!edito) return res.status(404).json({status: "not found"}); 
-    
-    return res.status(200).json({
-      status: "success", data: { product: products },
-  });
 }
   
   
-  exports.deleteProductById = (req, res) => {
-    const products = JSON.parse(
-      fs.readFileSync(`${__dirname}/../data/products.json`)
-      );
-    const position = products.findIndex(x => x.id == req.params.id);
-    products.splice(position, 1);
-
-    fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+  exports.deleteProductById = async(req, res) => {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    //console.log(product);
     res.status(200).json({
-      status: "Archivo borrado correctamente",
+      status: "Archivo borrado correctamente",  
+      product: product,
     });
   }
   
--- npmtest/server.js
+++ npmtest/server.js
@@ -5,10 +5,12 @@ const port = process.env.PORT || 3032;
 const url= 'mongodb://localhost:27017/fullstack';
 mongoose.connect(url, {}).then((con) => {
     console.log("Connected to mongo");
+    /*
     const p = new Product({ productName:"producto 2", price: 10});
     p.save().then(() => {
         console.log("saved");
     });
+    */
 });
 app.listen(port, () =>{
     console.log(`App running on port ${port}`);
