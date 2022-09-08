4a1d282cd4fd8a2d9c0f4926de21e6a0609df91c
Gisel Paye Luna <cecipaye@gmail.com>
2022-09-07 21:19:44 -0400

    actualizar

--- npmtest/server.js
+++ npmtest/server.js
@@ -1,5 +1,9 @@
+const mongoose = require("mongoose");
 const app = require("./app");
 const port = process.env.PORT || 3032;
+mongoose.connect(process.env.DATABASE, {}).then((con)=> {
+    console.log("Connected--------", con.connections);
+});
 app.listen(port, () =>{
     console.log(`App running on port ${port}`);
 });
