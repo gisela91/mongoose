769a2853364e409e7d19c192a90831e4b2541314
Gisel Paye Luna <cecipaye@gmail.com>
2022-09-10 19:12:32 -0400

    atualizar sequelize

--- npmtest/controllers/productController.js
+++ npmtest/controllers/productController.js
@@ -1,12 +1,9 @@
 const fs = require("fs");
+const { Product } = require("../models");
 //const path = require('path');
 
-exports.getAllProducts = (req, res) => {
-   // console.log(req.params);
-    const products = JSON.parse(
-      fs.readFileSync(`${__dirname}/../data/products.json`)
-      );
-    //console.log(req.requestTime);
+exports.getAllProducts = async(req, res) => {
+    const products = await Product.findAll();  
     res.status(200).json({
       status: "success",
       timeOfRequest: req.requestTime,
@@ -15,29 +12,21 @@ exports.getAllProducts = (req, res) => {
         products,
       },
     });
-  }
+  };
   
-  exports.addproduct = (req, res) => {
-    const products = JSON.parse(
-      fs.readFileSync(`${__dirname}/../data/products.json`)
-      );
-    products.push(req.body);
-    fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+  exports.addproduct = async(req, res) => {
+    let newProduct = Product.build(req.body);
+    newProduct = await newProduct.save();
     res.status(200).json({
       status: "success",
       data: {
-        products,
+        product: newProduct,
       },
     });
-  }
+  };
   
-  exports.getProductById = (req, res) => {
-    const products = JSON.parse(
-      fs.readFileSync(`${__dirname}/../data/products.json`)
-      );
-  
-    const foundProduct = products.find(p => p.id == req.params.id);
-    //console.log(req.params);
+  exports.getProductById = async(req, res) => {
+    const foundProduct = await Product.findByPk(req.params.id);
     if(foundProduct){
 
       res.status(200).json({
@@ -51,7 +40,7 @@ exports.getAllProducts = (req, res) => {
           status: "not found",
         });
     }
-  }
+  };
   
   exports.updateProductById = (req, res) => {
     const products = JSON.parse(fs.readFileSync(`${__dirname}/../data/products.json`));
