15ba155606afddd44975db40dea111226451a5bb
Gisel Paye Luna <cecipaye@gmail.com>
2022-09-11 20:54:43 -0400

    error

--- npmtest/app.js
+++ npmtest/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const productRouter = require("./routers/productRouters");
+const MyError = require("./utils/MyError");
 const app = express();
 //middlewers
 app.use(express.json());
@@ -12,4 +13,22 @@ app.use((req, res, next) => {
 //routers
 app.use("/api/v1/products", productRouter);
 
+app.all("*", (req, res, next)=> {
+  next(new MyError("route not found", 404))
+});
+
+app.use((err, req, res, next) => {
+  if(process.env.NODE_ENV == "development"){
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }else{
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: "ocurrio un error",
+    });
+  }
+  
+})
 module.exports = app;
